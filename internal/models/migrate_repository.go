3cd19f882837d500fcac482290b76dfceea9b16a
Li-Qing Wang <liqingnz@gmail.com>
2025-07-13 21:37:21 +0800

    fix: add event detector to migrate init

--- internal/models/migrate_repository.go
+++ internal/models/migrate_repository.go
@@ -22,6 +22,12 @@ func NewMigrateRepository(db *gorm.DB) *MigrateRepository {
 
 func (r *MigrateRepository) initMigrateList() {
 	r.migrateList = make(map[uint64]func() error)
+
+	// Migration 1: Create basic migration log table
+	r.migrateList[1] = r.createMigrationLogTable
+
+	// Migration 2: Create event detection tables
+	r.migrateList[2] = r.createEventDetectionTables
 }
 
 func (r *MigrateRepository) DoMigrate() error {
@@ -32,4 +38,17 @@ func (r *MigrateRepository) DoMigrate() error {
 		}
 	}
 	return nil
+}
+
+// Migration functions
+func (r *MigrateRepository) createMigrationLogTable() error {
+	return r.db.AutoMigrate(&MigrateLog{})
+}
+
+func (r *MigrateRepository) createEventDetectionTables() error {
+	return r.db.AutoMigrate(
+		&DetectedEvent{},
+		&EventScanState{},
+		&EventProcessingLog{},
+	)
 }
